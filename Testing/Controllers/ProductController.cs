1f8028a7d7be7ce3b4195a96e32d41b2d45c0312
damien young <dyoung1006@gmail.com>
2023-01-25 19:49:45 -0700

    all steps complete

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Testing.Models;
 
 namespace Testing.Controllers
 {
@@ -14,6 +16,48 @@ namespace Testing.Controllers
         {
             var products = repo.GetAllProducts();
             return View(products);
+        }
+
+        public IActionResult ViewProduct(int id)
+        {
+            var product = repo.GetProduct(id);
+            return View(product);
+        }
+
+        public IActionResult UpdateProduct(int id)
+        {
+            var product = repo.GetProduct(id);
+
+            if (product == null)
+            {
+                return View("ProductNotFound");
+            }
+            
+            return View(product);
+        }
+
+        public IActionResult UpdateProductToDatabase(Product product)
+        {
+            repo.UpdateProduct(product);
+            return RedirectToAction("ViewProduct", new { id = product.ProductID });
+        }
+
+        public IActionResult InsertProduct()
+        {
+            var prod = repo.AssignCategory();
+            return View(prod);
+        }
+
+        public IActionResult InsertProductToDatabase(Product productToInsert)
+        {
+            repo.InsertProduct(productToInsert);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteProduct(Product product) 
+        { 
+            repo.DeleteProduct(product);
+            return RedirectToAction("Index");
         }
     }
 }
